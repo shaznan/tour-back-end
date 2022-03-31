e68e12357f694763bb3ef3206a52df5ac6157121
shaznan123 <shaznanfairozesally@gmail.com>
2022-03-31 23:41:53 +0530

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const tourRoutes = require("./routes/tourRoutes");
+const cors = require("cors");
 
 if (process.env.NODE_ENV === "development") {
   console.log(`running in ${process.env.NODE_ENV} env`);
 }
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1/tours", tourRoutes);
